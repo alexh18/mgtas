e223dd8ee270c5d3d7239225b119f0d76d7ebc26
alexh18 <99298296+alexh18@users.noreply.github.com>
2022-06-07 08:59:24 -0500

    Add files via upload

---
+++ proyectoDisp-main/ProyectoAvanceParte2/ProyectoAvance/secciones.swift
@@ -0,0 +1,14 @@
+//
+//  secciones.swift
+//  ProyectoAvance
+//
+//  Created by UNAM FCA 11 on 07/04/22.
+//
+
+import Foundation
+import UIKit
+
+struct secciones {
+    var info : String
+    var imagen : UIImage
+}
